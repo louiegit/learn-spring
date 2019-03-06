cbf19ab1a77e5827fce6252b790b442b510a901f
luotianxiang <tianxiang.luo@hipac.cn>
2019-03-06 09:57:58 +0800

    commit

---
+++ transaction-tcc/src/main/java/com/learn/spring/transactiontcc/TransactionTccApplication.java
@@ -0,0 +1,13 @@
+package com.learn.spring.transactiontcc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionTccApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransactionTccApplication.class, args);
+    }
+
+}
---
+++ transaction-tcc/src/test/java/com/learn/spring/transactiontcc/TransactionTccApplicationTests.java
@@ -0,0 +1,16 @@
+package com.learn.spring.transactiontcc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TransactionTccApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
